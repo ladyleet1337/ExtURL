3506cc9d4543d5cc5212ccaf115c950fd7decfbb
R0taK <ryotak@wearehackerone.com>
2019-06-10 11:41:50 +0900

    Impl multi threading

--- ExtURL.go
+++ ExtURL.go
@@ -4,9 +4,11 @@ import (
 	"./util"
 	"flag"
 	"fmt"
+	"github.com/panjf2000/ants"
 	"mvdan.cc/xurls"
 	"strconv"
 	"strings"
+	"sync"
 )
 var(
 	baseURL      = flag.String("u","","Base URL to find URLs (required)")
@@ -16,7 +18,9 @@ var(
 	scanTargets []string
 	checkedURLs  = []string{*baseURL}
 	baseHost    string
+	wg sync.WaitGroup
 )
+var p *ants.PoolWithFunc
 func main(){
 	flag.Parse()
 	fmt.Println("[*] ExtURL v1.0 by RyotaK")
@@ -34,33 +38,22 @@ func main(){
 	scanTargets = append(scanTargets,xurls.Strict().FindAllString(string(response),-1)...)
 	fmt.Println("[*] Successfully connected to target url")
 	fmt.Println("[*] Starting extract url with "+strconv.Itoa(*threads)+" threads")
-	jobs := make(chan string, 512)
-	results := make(chan int, 512)
-	var executedJobs int
-	for w := 1; w <= *threads; w++ {
-		go worker(jobs, results)
-	}
+	pool,_ := ants.NewPoolWithFunc(*threads,func(url interface{}){
+		checkURL(url)
+		wg.Done()
+	})
+	defer pool.Release()
+	p = pool
 	for num := range scanTargets {
-		checkURL(scanTargets[num])
-		//executedJobs++
-		//jobs <- scanTargets[num]
+		wg.Add(1)
+		pool.Invoke(string(scanTargets[num]))
 	}
-	close(jobs)
-	for a := 1; a <= executedJobs; a++ {
-		<-results
-	}
+	wg.Wait()
+	fmt.Println("[*] Saving result to "+*outputPath)
 	util.WriteToFile(scanTargets,*outputPath)
 }
-
-func worker(URLs <-chan string, results chan<- int){
-	for URL := range URLs {
-		checkURL(URL)
-		results <- 0
-	}
-}
-
-func checkURL(URL string){
-	fmt.Println(URL)
+func checkURL(URLInterface interface{}){
+	var URL = URLInterface.(string)
 	if len(strings.Split(URL,"/")) >= 3 {
 		var host = strings.Split(URL, "/")[2]
 		if !util.ArrayContains(checkedURLs, URL) && !(*strictHost && host != baseHost) {
@@ -69,7 +62,10 @@ func checkURL(URL string){
 			var newURLs = xurls.Strict().FindAllString(string(response), -1)
 			scanTargets = util.AppendWithCheck(scanTargets, newURLs...)
 			for num := range scanTargets {
-				checkURL(scanTargets[num])
+				if !util.ArrayContains(checkedURLs, scanTargets[num]) {
+					wg.Add(1)
+					go p.Invoke(string(scanTargets[num]))
+				}
 			}
 		}
 	}
