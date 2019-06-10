ae75ff52e65935cee5c52b36eea18b129a5a4dd2
R0taK <ryotak@wearehackerone.com>
2019-06-10 14:28:45 +0900

    gofmt

--- ExtURL.go
+++ ExtURL.go
@@ -10,35 +10,37 @@ import (
 	"strings"
 	"sync"
 )
-var(
-	baseURL      = flag.String("u","","Base URL to find URLs (required)")
-	strictHost   = flag.Bool("s",true,"Whether to target only the same domain")
-	threads      = flag.Int("t",10,"Number of threads")
-	outputPath   = flag.String("o","output.txt","Output file path")
+
+var (
+	baseURL     = flag.String("u", "", "Base URL to find URLs (required)")
+	strictHost  = flag.Bool("s", true, "Whether to target only the same domain")
+	threads     = flag.Int("t", 10, "Number of threads")
+	outputPath  = flag.String("o", "output.txt", "Output file path")
 	scanTargets []string
-	checkedURLs  = []string{*baseURL}
+	checkedURLs = []string{*baseURL}
 	baseHost    string
-	wg sync.WaitGroup
+	wg          sync.WaitGroup
 )
 var p *ants.PoolWithFunc
-func main(){
+
+func main() {
 	flag.Parse()
 	fmt.Println("[*] ExtURL v1.0 by RyotaK")
-	if *baseURL == ""{
+	if *baseURL == "" {
 		flag.PrintDefaults()
 		return
 	}
 	fmt.Println("[*] Checking connection to target url")
 	var response = util.SendHTTPGet(*baseURL)
-	if response == nil{
+	if response == nil {
 		fmt.Println("[x] Failed to connect target")
 		return
 	}
-	baseHost = strings.Split(*baseURL,"/")[2]
-	scanTargets = append(scanTargets,xurls.Strict().FindAllString(string(response),-1)...)
+	baseHost = strings.Split(*baseURL, "/")[2]
+	scanTargets = append(scanTargets, xurls.Strict().FindAllString(string(response), -1)...)
 	fmt.Println("[*] Successfully connected to target url")
-	fmt.Println("[*] Starting extract url with "+strconv.Itoa(*threads)+" threads")
-	pool,_ := ants.NewPoolWithFunc(*threads,func(url interface{}){
+	fmt.Println("[*] Starting extract url with " + strconv.Itoa(*threads) + " threads")
+	pool, _ := ants.NewPoolWithFunc(*threads, func(url interface{}) {
 		checkURL(url)
 		wg.Done()
 	})
@@ -49,12 +51,12 @@ func main(){
 		pool.Invoke(string(scanTargets[num]))
 	}
 	wg.Wait()
-	fmt.Println("[*] Saving result to "+*outputPath)
-	util.WriteToFile(scanTargets,*outputPath)
+	fmt.Println("[*] Saving result to " + *outputPath)
+	util.WriteToFile(scanTargets, *outputPath)
 }
-func checkURL(URLInterface interface{}){
+func checkURL(URLInterface interface{}) {
 	var URL = URLInterface.(string)
-	if len(strings.Split(URL,"/")) >= 3 {
+	if len(strings.Split(URL, "/")) >= 3 {
 		var host = strings.Split(URL, "/")[2]
 		if !util.ArrayContains(checkedURLs, URL) && !(*strictHost && host != baseHost) {
 			checkedURLs = util.AppendWithCheck(checkedURLs, URL)
